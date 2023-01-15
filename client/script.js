37f857dabe89f4c1fabdf91be374cae680a882df
Shubhang Gupta <guptashubhang2000@gmail.com>
2023-01-16 02:05:43 +0530

    updated

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://codex-ndvm.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
